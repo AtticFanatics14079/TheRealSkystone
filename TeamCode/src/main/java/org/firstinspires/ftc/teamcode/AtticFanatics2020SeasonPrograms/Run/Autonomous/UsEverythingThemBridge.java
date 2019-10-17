c638fbab003f39c28eedf220dffcee86a24caae9
Vera Choi <kilanavila@gmail.com>
2019-10-17 19:04:22 -0400

    Merge branch 'master' of https://github.com/AtticFanatics14079/TheRealSkystone into Vera
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtticFanatics2020SeasonPrograms/ConfigureRobot.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtticFanatics2020SeasonPrograms/Referenced/Autonomous/NormalDrive.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtticFanatics2020SeasonPrograms/RobotNormalDriveNewFunctions.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtticFanatics2020SeasonPrograms/Run/Autonomous/TestingClassReferences.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtticFanatics2020SeasonPrograms/Run/Autonomous/UsEverythingThemBridge.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Run.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Referenced.Autonomous.MecanumDrive;
+
+public class UsEverythingThemBridge extends LinearOpMode {
+    MecanumDrive robot = new MecanumDrive();
+    @Override
+    public void runOpMode() throws InterruptedException{
+        waitForStart();
+
+    }
+}
