38b2cce4887554b7c090a9cc698d2c6ab914fa0e
Vera Choi <kilanavila@gmail.com>
2019-11-04 16:11:15 -0500

    telemetry descriptions for hitblock

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtticFanatics2020SeasonPrograms/Run/Autonomous/HitBlock.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtticFanatics2020SeasonPrograms/Run/Autonomous/HitBlock.java
@@ -4,6 +4,7 @@ import android.graphics.Camera;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Referenced.Autonomous.MecanumDrive;
 import org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Referenced.Configure;
 import org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Skystone.CameraDetect;
@@ -13,14 +14,19 @@ public class HitBlock extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         CameraDetect camera = new CameraDetect();
         MecanumDrive robot = new MecanumDrive();
+
         int sposition;
         waitForStart();
         robot.MoveEncoderTicks(79,1, hardwareMap);
         sposition = camera.getSkystone(hardwareMap);
+        telemetry.addData("xPosition: ", camera.getxPosition());
         switch(sposition){
-            case 0: robot.StrafeEncoderTicks(30, 1, hardwareMap); break;
-            case 1: robot.StrafeEncoderTicks(15,1,hardwareMap); break;
-            case 2: robot.StrafeEncoderTicks(45,1,hardwareMap); break;
+            case 0: robot.StrafeEncoderTicks(30, 1, hardwareMap);
+            telemetry.addLine("left"); break;
+            case 1: robot.StrafeEncoderTicks(15,1,hardwareMap);
+            telemetry.addLine("mid"); break;
+            case 2: robot.StrafeEncoderTicks(45,1,hardwareMap); telemetry.addLine("right");
+            break;
         }
         robot.MoveEncoderTicks(45,1,hardwareMap);
 
