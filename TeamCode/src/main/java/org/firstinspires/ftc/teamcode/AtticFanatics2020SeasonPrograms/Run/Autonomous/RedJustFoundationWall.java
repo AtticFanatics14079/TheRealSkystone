26986d48c10a2ba75867c8004cdf2958e3649f28
Anthony Zhao <anthonyrzhao@gmail.com>
2019-11-27 16:36:35 -0500

    RedFoundation

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtticFanatics2020SeasonPrograms/Run/Autonomous/RedJustFoundationWall.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Run.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Referenced.Autonomous.MecanumDrive;
+
+@Autonomous(name = "JustFoundationBlueSide")
+public class RedJustFoundationWall extends LinearOpMode {
+    MecanumDrive Robot = new MecanumDrive();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Robot.Configure(hardwareMap);
+        Robot.UnhookFoundation();
+        waitForStart();
+        Robot.Move(14, -0.9);
+        //Robot.Move(5);
+
+        Robot.Move(32);
+        Robot.Move(2, 0.3, false);
+        Robot.HookFoundation();
+        //Robot.Move(5, -0.8, false);
+        Robot.Motors[3].setPower(-1);
+        Robot.Motors[4].setPower(-1);
+        sleep(2000);
+        Robot.setPower(0, 0, 0);
+        //Not sure if this works
+        Robot.Move(3, 1);
+        Robot.Move(35, 0.6, false);
+        Robot.UnhookFoundation();
+        Robot.Move(13, 1);
+        Robot.Move(-45);
+    }
+}
