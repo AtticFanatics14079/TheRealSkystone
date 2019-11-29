6743b25e8ec56f6a95f3a51b578e7406aeaa52e5
JonCoombs <jonathan.d.coombs@gmail.com>
2019-11-29 16:36:06 -0500

    Please work, FIRST UNIT TEST!!!

--- TeamCode/src/test/java/org/firstinspires/ftc/teamcode/AtticFanatics2020SeasonPrograms/Referenced/Autonomous/MecanumDriveTest.groovy
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Referenced.Autonomous
-
-class MecanumDriveTest extends GroovyTestCase {
-    void testUnhookFoundation() {
-        assertEquals(1, 1);
-    }
-}
---
+++ TeamCode/src/testDebug/java/org/firstinspires/ftc/teamcode/AtticFanatics2020SeasonPrograms/Referenced/Autonomous/MecanumDriveTest.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Referenced.Autonomous;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MecanumDriveTest {
+
+    @Test
+    void unhookFoundation() {
+        assertEquals(1, 1);
+    }
+}
