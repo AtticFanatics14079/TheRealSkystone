e93222d37dcc91cee9f96599423c3b6e20b0a680
Anthony Zhao <anthonyrzhao@gmail.com>
2020-01-26 15:26:19 -0500

    Stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtticFanatics2020SeasonPrograms/Run/TeleOp/StatesTeleOp.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Run.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Referenced.TeleOp.Comp2TeleOpMecanum;
+import org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Referenced.TeleOp.StatesTeleOpMecanum;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Comp 2 TeleOp", group = "Sensor")
+public class StatesTeleOp extends OpMode {
+
+    StatesTeleOpMecanum Drive = new StatesTeleOpMecanum();
+
+    @Override
+    public void init() {
+        Drive.Configure(hardwareMap);
+        //Drive.RotateGripper.setPosition(Drive.ROTATE_GRIPPER_STRAIGHT);
+    }
+
+    @Override
+    public void loop() {
+            Drive.Move(hardwareMap, gamepad1, gamepad2);
+            /*telemetry.addData("Rotate Position: ", Drive.RotateGripper.getPosition());
+            telemetry.addData("Scissor Position: ", -Drive.Scissor.getCurrentPosition());
+            telemetry.addData("Extend Gripper Position: ", Drive.ExtendGripper.getCurrentPosition());
+            telemetry.addData("Foundation Left: ", Drive.FoundationLeft.getPosition());
+            telemetry.addData("Foundation Right: ", Drive.FoundationRight.getPosition());
+            telemetry.addData("Gripper: ", Drive.Gripper.getPosition());
+
+            */
+            telemetry.addData("Scissor Left", Drive.ScissorLeft.getCurrentPosition());
+            telemetry.addData("Scissor Right", Drive.ScissorRight.getCurrentPosition());
+            telemetry.addData("Level: ", Drive.level);
+            telemetry.addData("Gripper: ", Drive.Gripper.getPosition());
+            //telemetry.addData("Extend Gripper: ", Drive.ExtendGripper.getPower());
+            telemetry.update();
+    }
+}
