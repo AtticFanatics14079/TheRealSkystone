83057dc0f5970ea5dd5a42342564bddd51e21e10
Christopher Baron <cjbaron117@gmail.com>
2020-02-08 17:36:06 -0500

    update to RRpath middle(variable place based)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtticFanatics2020SeasonPrograms/Run/Autonomous/RROpModes/MassivePath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtticFanatics2020SeasonPrograms/Run/Autonomous/RROpModes/MassivePath.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Referenced.NoDriveConfigure;
-import org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Referenced.RoadRunner.SampleMecanumDriveBase;
+//import org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Referenced.RoadRunner.SampleMecanumDriveBase;
 import org.firstinspires.ftc.teamcode.AtticFanatics2020SeasonPrograms.Referenced.RoadRunner.SampleMecanumDriveREV;
 
 @Autonomous(group = "drive")
@@ -18,54 +18,63 @@ public class MassivePath extends LinearOpMode {
 
         NoDriveConfigure mech = new NoDriveConfigure();
         mech.Configure(hardwareMap);
+        Pose2d startPose = new Pose2d(-20.0,  -63.0, Math.toRadians(90.0));// changing this might make the path faster
+        Pose2d ingest1 = new Pose2d(-38.0,-24.0, Math.toRadians(135.0));
+        Pose2d ingest2 = new Pose2d(-47.0,-35.0, Math.toRadians(180.0));
+        Pose2d foundationgrab = new Pose2d(50.0,-25.0,Math.toRadians(270.0));
+        Pose2d foundationdump = new Pose2d(20.0,-40.0,Math.toRadians(180.0));
+        Pose2d foundationshove = new Pose2d(50.0,-40.0,Math.toRadians(180.0));
+        Pose2d middlepassage = new Pose2d(0.0, -40.0, Math.toRadians(180.0));
+        Pose2d parkposition = new Pose2d(0.0,-35.0, Math.toRadians(180.0));
 
-        drive.setPoseEstimate(new Pose2d(-20.0, -63.0, toRadians(90.0)));
+        drive.setPoseEstimate(startPose);
         Trajectory toStone1 = drive.trajectoryBuilder()
-                .splineTo(new Pose2d(-30.0,-24.0, toRadians(135))) // MIDDLE STONE POSITION
+                .splineTo(ingest1) // MIDDLE STONE POSITION
                 .build();
 
-        drive.setPoseEstimate(new Pose2d(-30.0, -24.0, toRadians(135)));
+        drive.setPoseEstimate(ingest1);
         Trajectory ingestStone1 = drive.trajectoryBuilder()
-                .strafeTo(new Vector2d(-40.0,-14.0))
-                .strafeTo(new Vector2d(-30.0, -24.0))
+                .splineTo (new Pose2d(ingest1.getX()-10.0, ingest1.getY()+10.0, ingest1.getHeading()))
+                .reverse()
+                .splineTo (ingest1)
                 .build();
 
         drive.setPoseEstimate(new Pose2d(-30.0,-24.0, toRadians(135)));
         Trajectory toFoundation1 = drive.trajectoryBuilder()
                 .reverse()
-                .splineTo(new Pose2d(0.0,-40.0, toRadians(180)))
-                .strafeTo(new Vector2d(40.0,-40.0))
-                .splineTo(new Pose2d(50.0,-33.0,toRadians(270)))
-                .strafeTo(new Vector2d(50.0,-25.0))
+                .splineTo(middlepassage)
+                .strafeTo(new Vector2d(middlepassage.getX()+40, middlepassage.getY()))
+                .splineTo(new Pose2d(foundationgrab.getX(), foundationgrab.getY()-8, foundationgrab.getHeading()))
+                .strafeTo(new Vector2d(foundationgrab.getX(),foundationgrab.getY()))
                 .build();
 
         drive.setPoseEstimate(new Pose2d(50.0,-25.0,toRadians(270)));
         Trajectory pullFoundation = drive.trajectoryBuilder()
-                .splineTo(new Pose2d(20.0,-40.0,toRadians(180)))
+                .splineTo(foundationdump)
                 .build();
 
         drive.setPoseEstimate(new Pose2d(20.0,-40.0,toRadians(180)));
         Trajectory toStone2 = drive.trajectoryBuilder()
-                .splineTo(new Pose2d(-38.0,-35.0,toRadians(135))) // MIDDLE STONE POSITION
+                .splineTo(ingest2) // MIDDLE STONE POSITION
                 .build();
 
         drive.setPoseEstimate(new Pose2d(-38.0, -35.0, toRadians(135)));
         Trajectory ingestStone2 = drive.trajectoryBuilder()
-                .strafeTo(new Vector2d(-46.0, -21.0))
-                .splineTo(new Pose2d(-57.0, -21.0, toRadians(180)))
-                .strafeTo(new Vector2d(-38.0, -35.0))
+                .strafeTo(new Vector2d(ingest2.getX()-10, ingest2.getY()+14))
+                .splineTo(new Pose2d(ingest2.getX()-16,ingest2.getY()+14, ingest2.getHeading()))
+                .strafeTo (ingest2.vec())
                 .build();
 
         drive.setPoseEstimate(new Pose2d(-38.0,-21.0,toRadians(180)));
         Trajectory toFoundation2 = drive.trajectoryBuilder()
                 .reverse()
-                .splineTo(new Pose2d(20.0,-40.0,toRadians(180)))
-                .strafeTo(new Vector2d(50.0,-40.0))
+                .splineTo(new Pose2d(foundationshove.getX()-30, foundationshove.getY(), foundationshove.getHeading()))
+                .strafeTo(new Vector2d(foundationshove.getX(),foundationshove.getY()))
                 .build();
 
         drive.setPoseEstimate(new Pose2d(50,-40,toRadians(180)));
         Trajectory park = drive.trajectoryBuilder()
-                .splineTo(new Pose2d(0.0,-35.0, toRadians(180)))
+                .splineTo(parkposition)
                 .build();
 
         //Trajectory dragFoundation = drive.trajectoryBuilder()
@@ -77,7 +86,7 @@ public class MassivePath extends LinearOpMode {
         drive.followTrajectorySync(toFoundation1);
         drive.followTrajectorySync(pullFoundation);
         drive.followTrajectorySync(toStone2);
-        drive.followTrajectorySync(toStone2);
+        drive.followTrajectorySync(ingestStone2);
         drive.followTrajectorySync(toFoundation2);
         drive.followTrajectorySync(park);
 
